34743ab9e9e0d327f72312a58e5525eab5d7366b
owopi <awesomebuilds.playmc@gmail.com>
2022-10-28 20:34:26 -0400

    created articles with forked potion api

---
+++ introduction-to-linux/script.js
@@ -0,0 +1,102 @@
+const tag_id = document.getElementById("nav");
+tag_id.innerHTML = `<nav class="navbar">
+<div class="logo"><span class="iconify" data-icon="flat-color-icons:linux"></span>VPCI Linux</div>
+<ul class="nav-links">
+    <input type="checkbox" id="checkbox_toggle" />
+    <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+    <div class="menu">
+        <li class="hover-target"><a href="">Home</a></li>
+        <li class="hover-target"><a href="">Join Us</a></li>
+        <li class="hover-target"><a href="">About Us</a></li>
+        <li class="hover-target"><a href="">All Posts</a></li>
+    </div>
+</ul>
+</nav>`;
+
+(function ($) {
+  "use strict";
+  document
+    .getElementsByTagName("body")[0]
+    .addEventListener("mousemove", function (n) {
+      (t.style.left = n.clientX + "px"),
+        (t.style.top = n.clientY + "px"),
+        (e.style.left = n.clientX + "px"),
+        (e.style.top = n.clientY + "px"),
+        (i.style.left = n.clientX + "px"),
+        (i.style.top = n.clientY + "px");
+    });
+  var t = document.getElementById("cursor"),
+    e = document.getElementById("cursor2"),
+    i = document.getElementById("cursor3");
+  function n(t) {
+    e.classList.add("hover"), i.classList.add("hover");
+  }
+  function s(t) {
+    e.classList.remove("hover"), i.classList.remove("hover");
+  }
+  s();
+  for (
+    var r = document.querySelectorAll(".hover-target"), a = r.length - 1;
+    a >= 0;
+    a--
+  ) {
+    o(r[a]);
+  }
+  function o(t) {
+    t.addEventListener("mouseover", n), t.addEventListener("mouseout", s);
+  }
+})(jQuery);
+
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    "use strict";
+    //Scroll back to top
+    var progressPath = document.querySelector(".progress-wrap path");
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "none";
+    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "stroke-dashoffset 10ms linear";
+    var updateProgress = function () {
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progress = pathLength - (scroll * pathLength) / height;
+      progressPath.style.strokeDashoffset = progress;
+    };
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 50;
+    var duration = 550;
+    jQuery(window).on("scroll", function () {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery(".progress-wrap").addClass("active-progress");
+      } else {
+        jQuery(".progress-wrap").removeClass("active-progress");
+      }
+    });
+    jQuery(".progress-wrap").on("click", function (event) {
+      event.preventDefault();
+      jQuery("html, body").animate({ scrollTop: 0 }, duration);
+      return false;
+    });
+  });
+})(jQuery);
+
+const notionDocId = "c2cf696984554e9891c9ca0a0295da75";
+
+//api is forked from https://github.com/benborgers/potion
+fetch("https://api.picafe.dev/html?id=" + notionDocId)
+  .then((res) => res.text())
+  .then((text) => {
+    console.log(text);
+    const trimmed = text.substring(
+      text.indexOf("<body>") + 6,
+      text.lastIndexOf("</body>")
+    );
+    console.log(trimmed);
+    document.querySelector("article").innerHTML = text;
+  });
